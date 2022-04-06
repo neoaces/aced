297661a75ca7d30f66b2506ea5f678952eeac4d1
anzlechavezz <anzlemchavez@gmail.com>
2022-04-06 19:07:33 -0400

    basic reformatting

--- aceproc/models.py
+++ aceproc/models.py
@@ -3,12 +3,13 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class CardSet(models.Model):
     set_title = models.CharField(max_length=50, default='Untitled Set')
     pub_date = models.DateTimeField('date set created', default=timezone.now)
     active = models.BooleanField('is the set active?', default=True)
-    
+
     def __str__(self) -> str:
         return self.set_title
 
@@ -16,8 +17,10 @@ class CardSet(models.Model):
 class Card(models.Model):
     question_text = models.CharField(default='', null=False, max_length=200)
     answer_text = models.CharField(default='', max_length=300)
-    created_date = models.DateTimeField('date card created', default=timezone.now)
+    created_date = models.DateTimeField(
+        'date card created', default=timezone.now
+    )
     rel_cardset = models.ForeignKey(CardSet, on_delete=models.CASCADE)
-    
+
     def __str__(self) -> str:
         return self.question_text
--- aceproc/serializers.py
+++ aceproc/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from .models import CardSet
+from .models import Card
 
 
 # Create a serializer as a class
 class CardSerializer(serializers.ModelSerializer):
     class Meta:  # Set the metadata
-        model = CardSet
-        fields = ('set_title', 'pub_date', 'active')
+        model = Card
+        fields = (
+            'question_text', 'answer_text', 'created_date', 'rel_cardset'
+        )
