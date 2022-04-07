7b0789b69ee30ce8005d94eb02713dcb4ea42945
anzlechavezz <anzlemchavez@gmail.com>
2022-04-06 20:26:00 -0400

    part done; need to add TODO

--- ace-frontend/src/App.tsx
+++ ace-frontend/src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
 import './index.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className='text-3xl'>hello</h1>
-      </header>
-    </div>
-  );
+// Since React.Component automatically has a type of <Readonly {}>,
+// a type must be made to define the props and the state.
+// TODO: Add a type for the query that is returned / HINT: in the console
+
+export default class App extends Component<{}, { todo_list: Array<any> }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      todo_list: [],
+    };
+  }
+
+  // Everytime that the page is loaded, fetch again from Django.
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      // Gets the response from the Django webserver
+      .get("main/cards")
+      // Deserializes the info given from serializer.py
+      .then(res => this.setState({ todo_list: res.data }))
+      // Catches any data error
+      .catch(err => console.log(err))
+
+  }
+
+  // React component that returns all the cards in the list.
+  renderItems = () => {
+    const items = this.state.todo_list;
+    console.log(this.state.todo_list);
+    return items.map((item, index) => (
+      <p key={index}> {item.question_text} </p >
+    ))
+  }
+
+  render() {
+    return (
+      <div className='p-5'>
+        {this.renderItems()}
+      </div>
+    )
+  }
 }
-
-export default App;
--- ace-frontend/src/index.tsx
+++ ace-frontend/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// 1. Element container of the react code <- HTML
+// NOTE: the exclamation mark prevents the error saying that the 
+// element returned might be null.
+const container = document.getElementById('root')!
+
+// 2. Create a root; React's way of rendering elements into HTML
+const root = ReactDOMClient.createRoot(container) // <- ERROR RAISED HERE
+
+// 3. Render the elements onto the root.
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
